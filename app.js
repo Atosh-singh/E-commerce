8100060c8b46579b451eb8a9b0ce0ea237a2effe
Atosh-singh <atoshsingh7536@gmail.com>
2023-02-01 21:23:03 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-const http = require("http");
+//const http = require("http");
 
-const routes = require("./routes.js");
+const express = require("express");
 
-const server = http.createServer(routes);
+const bodyParser = require("body-parser");
 
-server.listen(3000);
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    `<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>`
+  );
+});
+
+app.post("/product", (res, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express.js</h1>");
+});
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+
+app.listen(3000);
