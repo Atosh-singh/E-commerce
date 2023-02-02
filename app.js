2648c7bedab19ceaffce1c3476fd9fc2db8d39c9
Atosh-singh <atoshsingh7536@gmail.com>
2023-02-02 11:14:25 +0530

    shop item

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
-//const http = require("http");
-
 const express = require("express");
 
 const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    `<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>`
-  );
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.post("/product", (res, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found!</h1>");
 });
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express.js</h1>");
-});
-
 // const server = http.createServer(app);
 
 // server.listen(3000);
