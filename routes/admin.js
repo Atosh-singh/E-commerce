423a61f42aa1175d4390cb358f5c101c64528dad
Atosh-singh <atoshsingh7536@gmail.com>
2023-02-09 11:06:04 +0530

    pug

--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,10 @@ const products = [];
 router.get("/add-product", (req, res, next) => {
   // res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
   // res.sendFile(path.join(rootDir, "views", "add-product.html"));
-  res.render("add-product.pug", { pageTitle: "Add Product" });
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
 // /admin/add-product => POSt
