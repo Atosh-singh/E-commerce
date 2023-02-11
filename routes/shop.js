fef13b74ad15c1231f1cac41169d2d3695a31470
Atosh-singh <atoshsingh7536@gmail.com>
2023-02-11 11:32:09 +0530

    handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,6 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../util/path");
-
 const adminData = require("./admin");
 
 const router = express.Router();
@@ -11,7 +10,15 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   //  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
   const products = adminData.products;
-  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+    layout: false,
+  });
 });
 
 module.exports = router;
