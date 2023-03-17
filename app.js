56b29a9bda9f820d5d72620779f264654fd873d0
Atosh-singh <atoshsingh7536@gmail.com>
2023-03-17 11:02:37 +0530

    cart

--- app.js
+++ app.js
@@ -4,27 +4,26 @@ const express = require("express");
 
 const bodyParser = require("body-parser");
 
+const errorController = require("./controllers/error.js");
+
 const app = express();
 
 app.set("view engine", "ejs");
 //app.set("view engine", "pug");
 app.set("views", "views");
 
-const adminData = require("./routes/admin.js");
+const adminRoutes = require("./routes/admin.js");
 const shopRoutes = require("./routes/shop.js");
-const exp = require("constants");
+//const exp = require("constants");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, "views", "error.html"));
-  res.status(404).render("404", { pageTitle: "Page Not Found!" });
-});
+app.use(errorController.get404);
 // const server = http.createServer(app);
 
 // server.listen(3000);
